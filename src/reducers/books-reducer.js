7b1c84665c664df189712fd4c014445fa760a103
Kirill <kirill.morozov_1994@mail.ru>
2020-07-17 10:40:01 +0300

    Change initial data books

--- src/reducers/books-reducer.js
+++ src/reducers/books-reducer.js
@@ -19,35 +19,10 @@ const initialState = [
 	},
 	{
 		id: 3,
-		title: 'Production-Ready Microservices',
-		author: 'Susan J. Fowler',
+		title: 'React и Redux. Функциональная веб-разработка',
+		author: 'Бэнкс Алекс, Порселло Ева',
 		year: 2018,
-		coverImage:
-			'https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg',
-	},
-	{
-		id: 4,
-		title: 'Release It!',
-		author: 'Michael T. Nygard',
-		year: 2017,
-		coverImage:
-			'https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg',
-	},
-	{
-		id: 5,
-		title: 'Production-Ready Microservices',
-		author: 'Susan J. Fowler',
-		year: 2018,
-		coverImage:
-			'https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg',
-	},
-	{
-		id: 6,
-		title: 'Release It!',
-		author: 'Michael T. Nygard',
-		year: 2017,
-		coverImage:
-			'https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg',
+		coverImage: 'https://cdn1.ozone.ru/multimedia/c650/1021367273.jpg',
 	},
 ]
 
