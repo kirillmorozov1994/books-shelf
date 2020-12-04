5435928a6f79ab664664fa3776c241e0dfd138f5
Kirill <kirill.morozov_1994@mail.ru>
2020-12-04 17:49:41 +0300

    Updates

---
+++ precache-manifest.9ec996bc473e41aef578f03a145047de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "052ba667aba0242c2234f89893285a8a",
+    "url": "/books-shelf/index.html"
+  },
+  {
+    "revision": "f3d1b3a3f970ff69a903",
+    "url": "/books-shelf/static/css/main.698a1e47.chunk.css"
+  },
+  {
+    "revision": "df1788bdbaca5989dd28",
+    "url": "/books-shelf/static/js/2.af02146d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/books-shelf/static/js/2.af02146d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d1b3a3f970ff69a903",
+    "url": "/books-shelf/static/js/main.c4b2cc71.chunk.js"
+  },
+  {
+    "revision": "96f1d026d297055ce485",
+    "url": "/books-shelf/static/js/runtime-main.ead18b64.js"
+  },
+  {
+    "revision": "cf72433fe60fbf55f662b15ee2b913c0",
+    "url": "/books-shelf/static/media/error-404.cf72433f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a2ed136f4c07733245822142526ee505.js"
+  "/books-shelf/precache-manifest.9ec996bc473e41aef578f03a145047de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/books-shelf/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
